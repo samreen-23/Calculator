d988b9e2550a3014671b53eab080830e012127ee
samreen-23 <samreen23@navgurukul.org>
2024-08-01 13:44:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// obj = {
+//     name:"samreen",
+//     age:20,
+//     number:123445
+// }
+// console.log(obj.name);
+
+
+// let num = 123;
+// let str = num.toString();
+
+// console.log(typeof str);
+
+// let arr = [1, 2, 3];
+// let str = arr.toString();
+
+
+// let obj = {name: "Alice"};
+// let str = obj.toString(); // "[object Object]"
+
+// console.log(obj.name);
+
+
+let str = "radhika";
+let result = str.slice(-6, -1); // "World"
+console.log(result);
+
